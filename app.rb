887ab7ef016208a336e78785fb84e43272df0633
Charles-Spaanheden <charliespaan@gmail.com>
2025-02-18 10:12:27 +0100

    Login/register upd

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'sqlite3'
 require 'sinatra/reloader'
 require 'bcrypt'
 
+enable :sessions
+
 get('/') do
     # db = SQLite3::Database.new("db/databas.db")
     # @image = db.execute("SELECT img FROM card")
@@ -42,3 +44,47 @@ post('/cards/create') do
     redirect("/cards")
 end
 
+get('/cards/register') do
+    slim(:"cards/register")
+end
+
+post('/cards/register') do
+    username = params[:username]
+    password = params[:password]
+    password_confirmation = params[:password_confirmation]
+    if password == password_confirmation
+        password_digest = BCrypt::Password.create(password)
+        db = SQLite3::Database.new("db/databas.db")
+        db.results_as_hash = true
+        db.execute("INSERT INTO user (username,password) VALUES (?,?)",[username,password_digest])
+        session[:username] = username
+        p username
+        p session[:username]
+        redirect("/")
+    else
+        p "Passwords do not match"
+        redirect("/cards/register")
+    end
+end
+
+get('/cards/login') do
+    slim(:"cards/login")
+end
+
+post('/cards/login') do
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new("db/databas.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM user WHERE username = ?",username).first
+    pwdigest = result["password"]
+    if BCrypt::Password.new(pwdigest) == password
+        p "Inloggad"
+        session[:username] = username
+        redirect("/")
+        p session[:username]
+    else
+        p "Fel lösenord"
+        redirect("/cards/login")
+    end
+end
